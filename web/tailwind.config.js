a8053f2533dc42708ed735e64c31b565cd478141
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-05-25 13:00:05 -0300

    :wrench: Configs

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,16 +4,24 @@
 */
 
 module.exports = {
-  content: ['./src/**/*.tsx'], // aqui pode ser necessário alterar a extensão para a extensão que vc tiver usando
-  theme: {
-    extend: {
-      // tem como vc extender e adicionar novas cores no tailwind por aqui
-      colors: {
-        brand: {
-          500: '#8257e6',
-        },
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.tsx'], // aqui pode ser necessário alterar a extensão para a extensão que vc tiver usando
+	theme: {
+		extend: {
+			// tem como vc extender e adicionar novas cores no tailwind por aqui
+			colors: {
+				brand: {
+					300: '#996dff',
+					500: '#8257e6'
+				}
+			},
+			borderRadius: {
+				md: '4px'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')]
+};
+
+/* 
+  os plugins do tailwind adiciona meio que mais estilos aqui esta sendo usado o de forms e tailwind-scrollbar para estilizar a scrollbar
+*/
