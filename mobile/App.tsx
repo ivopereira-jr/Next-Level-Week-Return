b6de6e090572b1cf46706f729a47c936f81d3ecc
ivopereira-jr <ivopereira.jr12@gmail.com>
2022-05-30 14:33:18 -0300

    :sparkles: Adicionando fonte inter

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,43 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import { Widget } from './src/components/Widget';
+
+import { theme } from './src/theme';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	SplashScreen.preventAutoHideAsync();
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	SplashScreen.hideAsync();
+
+	return (
+		<View style={styles.container}>
+			<Widget />
+			<StatusBar style="light" backgroundColor="transparent" translucent />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: theme.colors.background,
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
